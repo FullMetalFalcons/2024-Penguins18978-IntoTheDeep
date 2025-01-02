6ec5b931703d3c654aecb6eca266f001eb41be66
Stephen <stephen@tinefamily.com>
2025-01-02 11:25:45 -0500

    Modified the current auto route to use a function for scoring a specimen, eliminating repeated code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimenScoreRoute.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimenScoreRoute.java
@@ -15,34 +15,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Config
 @Autonomous
 public class SpecimenScoreRoute extends LinearOpMode {
+
+    // Set up constants for the size of the robot
+    int botWidth = 18;
+
+    // Set up constants for "preset" field locations
+    int STARTING_POSITION_Y = -70 + (botWidth/2);
+    int STARTING_POSITION_X = 9;
+
+    int SCORING_POSITION_X = STARTING_POSITION_X;
+    int SCORING_POSITION_Y = STARTING_POSITION_Y + 12;
+
+    int PICKUP_POSITION_X = 40;
+    int PICKUP_POSITION_Y = STARTING_POSITION_Y + 3;
+
+    int PARKING_POSITION_X = 48;
+    int PARKING_POSITION_Y = PICKUP_POSITION_Y;
+
+    PinpointDrive drive = null;
+    PenguinsArm arm = null;
+
     public void runOpMode() {
-        // Set up constants for the size of the robot
-        int botWidth = 18;
 
-        // Set up constants for "preset" field locations
-        int STARTING_POSITION_Y = -70 + (botWidth/2);
-        int STARTING_POSITION_X = 9;
-
-        int SCORING_POSITION_X = STARTING_POSITION_X;
-        int SCORING_POSITION_Y = STARTING_POSITION_Y + 12;
-
-        int PICKUP_POSITION_X = 40;
-        int PICKUP_POSITION_Y = STARTING_POSITION_Y + 3;
-
-        int PARKING_POSITION_X = 48;
-        int PARKING_POSITION_Y = PICKUP_POSITION_Y;
-
-
-        PinpointDrive drive = new PinpointDrive(hardwareMap, new Pose2d(STARTING_POSITION_X, STARTING_POSITION_Y, Math.toRadians(90)));
-        PenguinsArm arm = new PenguinsArm(hardwareMap, telemetry);
+        drive = new PinpointDrive(hardwareMap, new Pose2d(STARTING_POSITION_X, STARTING_POSITION_Y, Math.toRadians(90)));
+        arm = new PenguinsArm(hardwareMap, telemetry);
 
         // Close the claw on initialization
         arm.setClawPosition(arm.CLAW_CLOSED);
 
-
         Action fullScoringTrajectory;
-        Action scoreSpecimenSequence;
-        Action scoreSpecimenSequence2;
 
         fullScoringTrajectory = drive.actionBuilder(drive.pose)
                 .waitSeconds(0.5)
@@ -58,52 +59,45 @@ public class SpecimenScoreRoute extends LinearOpMode {
                 .splineToLinearHeading(new Pose2d( PARKING_POSITION_X, PARKING_POSITION_Y, Math.toRadians(180)), 0)
                 .build();
 
-        scoreSpecimenSequence =
-                new SequentialAction(
-                    new ParallelAction(
-                            arm.armToPosition(arm.ARM_SPECIMEN_READY_DEGREES, arm.SLIDE_RESET_INCHES),
-                            drive.actionBuilder(drive.pose).strafeToLinearHeading(new Vector2d( SCORING_POSITION_X, SCORING_POSITION_Y ), Math.toRadians(90)).build()
-                    ),
-                    arm.armToPosition(arm.ARM_SPECIMEN_READY_DEGREES, arm.SLIDE_SPECIMEN_READY_INCHES),
-                    arm.armToPosition(arm.ARM_SPECIMEN_SCORE_DEGREES, arm.SLIDE_SPECIMEN_READY_INCHES),
-                    arm.armToPosition(arm.ARM_SPECIMEN_SCORE_DEGREES, arm.SLIDE_SPECIMEN_SCORE_INCHES),
-                    arm.clawToPosition(arm.CLAW_OPEN),
-                    new SleepAction(0.2),
-                    arm.armToPosition(arm.ARM_RESET_DEGREES, arm.SLIDE_RESET_INCHES)
-                );
+        Action scoringRoute1 = getNewScoringAction(0);
 
-        scoreSpecimenSequence2 =
-                new SequentialAction(
-                        new ParallelAction(
-                                arm.armToPosition(arm.ARM_SPECIMEN_READY_DEGREES, arm.SLIDE_RESET_INCHES),
-                                drive.actionBuilder(drive.pose).strafeToLinearHeading(new Vector2d( SCORING_POSITION_X - 6, SCORING_POSITION_Y ), Math.toRadians(90)).build()
-                        ),
-                        arm.armToPosition(arm.ARM_SPECIMEN_READY_DEGREES, arm.SLIDE_SPECIMEN_READY_INCHES),
-                        arm.armToPosition(arm.ARM_SPECIMEN_SCORE_DEGREES, arm.SLIDE_SPECIMEN_READY_INCHES),
-                        arm.armToPosition(arm.ARM_SPECIMEN_SCORE_DEGREES, arm.SLIDE_SPECIMEN_SCORE_INCHES),
-                        arm.clawToPosition(arm.CLAW_OPEN),
-                        new SleepAction(0.2),
-                        arm.armToPosition(arm.ARM_RESET_DEGREES, arm.SLIDE_RESET_INCHES)
-                );
+        Action scoringRoute2 = getNewScoringAction(-6);
 
+        Action scoringRoute3 = getNewScoringAction(-12);
 
 
         waitForStart();
         if (isStopRequested()) return;
 
+
         Actions.runBlocking(
                 new SequentialAction(
                         // Score pre-loaded specimen
-                        scoreSpecimenSequence,
+                        scoringRoute1,
                         // Drive to get another
                         drive.actionBuilder(drive.pose).splineToLinearHeading(new Pose2d( PICKUP_POSITION_X, PICKUP_POSITION_Y, 0), 0).build(),
                         arm.clawToPosition(arm.CLAW_CLOSED),
                         new SleepAction(0.75),
-                        // Score the newly grabbed specimen
-                        scoreSpecimenSequence2,
+                        // Score the newly grabbed specimenMo
+                        scoringRoute2,
                         // Park in the Observation Zone
                         drive.actionBuilder(drive.pose).splineToLinearHeading(new Pose2d( PARKING_POSITION_X, PARKING_POSITION_Y, Math.toRadians(180)), 0).build()
                 )
+        );
+    }
+
+    public Action getNewScoringAction(int barOffsetX) {
+        return new SequentialAction(
+                new ParallelAction(
+                        arm.armToPosition(arm.ARM_SPECIMEN_READY_DEGREES, arm.SLIDE_RESET_INCHES),
+                        drive.actionBuilder(drive.pose).strafeToLinearHeading(new Vector2d( SCORING_POSITION_X + barOffsetX, SCORING_POSITION_Y ), Math.toRadians(90)).build()
+                ),
+                arm.armToPosition(arm.ARM_SPECIMEN_READY_DEGREES, arm.SLIDE_SPECIMEN_READY_INCHES),
+                arm.armToPosition(arm.ARM_SPECIMEN_SCORE_DEGREES, arm.SLIDE_SPECIMEN_READY_INCHES),
+                arm.armToPosition(arm.ARM_SPECIMEN_SCORE_DEGREES, arm.SLIDE_SPECIMEN_SCORE_INCHES),
+                arm.clawToPosition(arm.CLAW_OPEN),
+                new SleepAction(0.2),
+                arm.armToPosition(arm.ARM_RESET_DEGREES, arm.SLIDE_RESET_INCHES)
         );
     }
 }
