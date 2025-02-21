20e917810325f00fca9839329f137b2ef14656c8
Stephen <stephen@tinefamily.com>
2025-02-21 14:13:40 -0500

    Added a manual override to the HuskyLens alliance detection (by pressing a)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TripleSpecimenRoute.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TripleSpecimenRoute.java
@@ -78,7 +78,7 @@ public class TripleSpecimenRoute extends LinearOpMode {
 
         sleep(100);
         // Adjust robot positioning based on which color specimen is pre-loaded
-        if (getHuskyLensColor() == SpecimenScoreRoute.HuskyColors.BLUE) {
+        if (getHuskyLensColor() == SpecimenScoreRoute.HuskyColors.BLUE || gamepad1.a) {
             scoringPositionYAddon = 11;
             Light.setPosition(LED_BLUE);
         } else {
